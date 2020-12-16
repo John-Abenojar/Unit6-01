9214e623eb49061559e1c3d5f379d6a50e7bfe0b
JohnAbenojar <john.kirby.abenojar@ocsbstudent.ca>
2020-12-16 15:18:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var number=prompt("Enter a number from 1-6")
+if(number=="2"){
+  alert ("correct!");
+}
+else {
+  alert ("wrong!");
+}
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
